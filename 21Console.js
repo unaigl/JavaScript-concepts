58ab9ed9a51988ac767e90f236aa7a5509f90afe
unaigl <unaiarki@gmail.com>
2022-06-24 13:05:02 +0200

    concepts

--- 21Console.js
+++ 21Console.js
@@ -3,3 +3,23 @@ const arr2 = [6, 7, 8, 9, 10];
 const arr3 = [11, 12, 13, 14, 15];
 
 console.table(arr1, arr2, arr3);
+
+Console.assert();
+Console.count();
+Console.debug()(en - US);
+Console.dir();
+Console.dirxml();
+Console.error();
+Console._exception();
+Console.group()(en - US);
+Console.groupCollapsed()(en - US);
+Console.groupEnd()(en - US);
+Console.info();
+Console.log();
+Console.profile()(en - US);
+Console.profileEnd()(en - US);
+Console.table();
+Console.time();
+Console.timeEnd();
+Console.timeStamp()(en - US);
+Console.trace();
