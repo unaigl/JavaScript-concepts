a96993a69794d7891843e0206778564da641ebf3
unaigl <unaiarki@gmail.com>
2022-07-05 10:37:53 +0200

    concepts

--- js.js
+++ js.js
@@ -1,4 +1,24 @@
-function palindrome2() {}
+function palindrome2(str) {
+  const str1 = str.split("");
+  if (!str1.length % 2) {
+    var strA1 = [];
+    var strA2 = [];
+    for (let i = 0; i < str1.length / 2; i++) {
+      strA1[i] = str1[i];
+      strA2[i] = str1[str1.length - 1 - i];
+    }
+    return strA1.join("") === strA2.join("");
+  } else {
+    const lengthArr2 = (str1.length - 1) / 2;
+    var strB1 = [];
+    var strB2 = [];
+    for (let i = 0; i < lengthArr2; i++) {
+      strB1[i] = str1[i];
+      strB2[i] = str1[str1.length - 1 - i];
+    }
+    return strB1.join("") === strB2.join("");
+  }
+}
 
 console.log(palindrome2("radar")); //true
 console.log(palindrome2("redder")); //true
--- studyCases/16Palindromes.js
+++ studyCases/16Palindromes.js
@@ -38,3 +38,26 @@ let palindrome2 = function (word) {
 console.log(palindrome2("radar")); //true
 console.log(palindrome2("redder")); //true
 console.log(palindrome2("windows")); //false
+
+// Option 3, using for
+function palindrome3(str) {
+  const str1 = str.split("");
+  if (!str1.length % 2) {
+    var strA1 = [];
+    var strA2 = [];
+    for (let i = 0; i < str1.length / 2; i++) {
+      strA1[i] = str1[i];
+      strA2[i] = str1[str1.length - 1 - i];
+    }
+    return strA1.join("") === strA2.join("");
+  } else {
+    const lengthArr2 = (str1.length - 1) / 2;
+    var strB1 = [];
+    var strB2 = [];
+    for (let i = 0; i < lengthArr2; i++) {
+      strB1[i] = str1[i];
+      strB2[i] = str1[str1.length - 1 - i];
+    }
+    return strB1.join("") === strB2.join("");
+  }
+}
